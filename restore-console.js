347b9b91d1e3a483002f59633b96852c16e26474
Alexey Movchan <a.movchan@elfsight.com>
2025-05-20 11:11:13 +0300

    collback

--- restore-console.js
+++ restore-console.js
@@ -1,10 +1,12 @@
-const iframe = document.createElement("iframe");
-iframe.style.display = "none";
-document.body.appendChild(iframe);
+(() => {
+  const iframe = document.createElement("iframe");
+  iframe.style.display = "none";
+  document.body.appendChild(iframe);
 
-const originalConsole = iframe.contentWindow.console;
-for (let key in originalConsole) {
-  if (typeof originalConsole[key] === "function") {
-    console[key] = originalConsole[key].bind(window);
+  const originalConsole = iframe.contentWindow.console;
+  for (let key in originalConsole) {
+    if (typeof originalConsole[key] === "function") {
+      console[key] = originalConsole[key].bind(window);
+    }
   }
-}
+})();
